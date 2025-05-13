5b54705bf870ad58fc7ddc5e1aa459fc34339e9b
yukidoi <doi_y@vitallead.co.jp>
2025-05-13 11:42:24 +0900

    Make page of user list

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(:id).page(params[:page]).per(10)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to users_path, alert: t('errors.record_not_found', model: User.model_name.human)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
+  resources :users
   resources :books
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-   
+  
   root to: 'home#index'
 end
