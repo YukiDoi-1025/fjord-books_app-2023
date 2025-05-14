c923d573610bbfc25514c16ce8d9e8b7991a9f3e
yukidoi <doi_y@vitallead.co.jp>
2025-05-14 13:20:47 +0900

    Add function to upload user icon image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validate :avatar_content_type, if: :was_attached?
+
+  def avatar_content_type
+    extension = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
+    errors.add(:avatar, 'に設定できるファイルの拡張子はjpg, png, gifです。') unless avatar.content_type.in?(extension)
+  end
+
+  def was_attached?
+    avatar.attached?
+  end
 end
