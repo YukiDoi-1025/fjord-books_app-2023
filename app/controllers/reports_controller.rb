db3771388278f2f5557683b4a8eaf9b7bd396844
yukidoi <doi_y@vitallead.co.jp>
2025-05-28 13:27:11 +0900

    Impliment save function in model

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -20,7 +20,6 @@ class ReportsController < ApplicationController
 
   def create
     @report = current_user.reports.new(report_params)
-    view_context.make_mentions(@report)
 
     if @report.save
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
@@ -31,7 +30,6 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      view_context.make_mentions(@report)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
--- app/models/report.rb
+++ app/models/report.rb
@@ -19,11 +19,21 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_save :make_mentions
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  def make_mentions
+    mentioning_relationships.destroy_all
+    content.scan(%r{http://localhost:3000/reports/\d+}).each do |url|
+      mentioned_id = url.split('/')[-1]
+      ReportMention.create(mentioning_id: id, mentioned_id:)
+    end
   end
 end
